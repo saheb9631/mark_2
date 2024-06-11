8335e6bac8611756b22fec5dccb6f4227483d093
saheb9631 <92590505+saheb9631@users.noreply.github.com>
2024-06-11 09:35:37 +0530

    Add files via upload

---
+++ main.js
@@ -0,0 +1,8 @@
+var typed = new Typed(".text",{
+    strings:["Android Developer", "Cloud Engineer", "Frontend Developer","YouTuber"],
+    typeSpeed : 10,
+    backSpeed : 100,
+    backDelay : 1000,
+    loop : true
+});
+Frontend
